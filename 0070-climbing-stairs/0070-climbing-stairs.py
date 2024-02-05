72578c687b7fa0cfc009f98d6bb0a1be2a7a7c47
MD Rashid Shahariar Shocho <133902894+Rashid-Shocho@users.noreply.github.com>
2024-02-05 19:22:54 +0600

    Time: 46 ms (7.92%), Space: 16.6 MB (76.37%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        n = n+1
+        
+        cache = {0: 0, 1: 1}
+
+        def fibonacci_of(n):
+            if n in cache:  # Base case
+              return cache[n]
+         # Compute and cache the Fibonacci number
+            cache[n] = fibonacci_of(n - 1) + fibonacci_of(n - 2)  # Recursive case
+            return cache[n]
+ 
+        
+        return fibonacci_of(n)
